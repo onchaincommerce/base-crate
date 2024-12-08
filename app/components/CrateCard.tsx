8d6fc13954a674085c7f6763c783ee214c60edf3
Aus Heller <aus@onchaincommerce.io>
2024-12-08 13:55:12 -0800

    Simplify CheckoutButton to basic implementation

--- app/components/CrateCard.tsx
+++ app/components/CrateCard.tsx
@@ -128,8 +128,6 @@ export default function CrateCard({ type, isSelected, onClick, productId, onStat
       {isSelected && hasMinBalance ? (
         <Checkout productId={productId} onStatus={onStatus}>
           <CheckoutButton
-            coinbaseBranded
-            label="Pay with USDC"
             className="w-full bg-gradient-to-r from-csgo-blue to-csgo-purple-light
                      py-3 px-6 rounded
                      text-white font-bold uppercase text-sm tracking-wider
